4bb8473dd9004034729729692107f8353a70d1f3
Helen Cris <helencristavares@gmail.com>
2024-03-31 23:53:29 -0300

    ✨ feat: created post and comment services

---
+++ src/services/commentsService.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { IComments } from './comments.interface';
+import { ReactNode } from 'react';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const getCommentById = async (id: ReactNode) => {
+	const data = await axios.get<IComments[]>(`${baseUrl}/posts/${id}/comments`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	return data;
+};
+
+export const createComment = async (comment: IComments) => {
+	const data = await axios.post<IComments[]>(`${baseUrl}/comments`, comment);
+	return data;
+};
---
+++ src/services/postService.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { IPost } from './post.interface';
+import { ReactNode } from 'react';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const getAllPosts = async () => {
+	const data = await axios.get<IPost[]>(`${baseUrl}/posts`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	return data;
+};
+
+export const createPost = async (post: IPost) => {
+	const data = await axios.post<IPost[]>(`${baseUrl}/posts`, post);
+	return data;
+};
+
+export const deletePost = async (id: ReactNode) => {
+	const data = await axios.delete<IPost[]>(`${baseUrl}/posts/${id}`, {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	return data;
+};
